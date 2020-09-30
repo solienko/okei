6fd78e5c66c2a666b199f3131984bb2415e7905d
Edward Solienko <solienko@hotmail.com>
2020-09-30 15:38:52 +0300

    Add files via upload

---
+++ sql/OKEIGroups.sql
@@ -0,0 +1,22 @@
+﻿/* Международные единицы измерения, включенные в ОКЕИ */
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (1, N'Единицы длины', 1)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (2, N'Единицы площади', 1)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (3, N'Единицы объема', 1)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (4, N'Единицы массы', 1)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (5, N'Технические единицы', 1)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (6, N'Единицы времени', 1)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (7, N'Экономические единицы', 1)
+
+/* Национальные единицы измерения, включенные в ОКЕИ */
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (8, N'Единицы длины', 2)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (9, N'Единицы площади', 2)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (10, N'Единицы объема', 2)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (11, N'Единицы массы', 2)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (12, N'Технические единицы', 2)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (13, N'Единицы времени', 2)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (14, N'Экономические единицы', 2)
+
+/* Четырехзначные национальные единицы измерения, включенные в ОКЕИ */
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (15, N'Технические единицы', 3)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (16, N'Экономические единицы', 3)
+INSERT [OKEIGroups] ([Id], [Name], [SectionId]) VALUES (17, N'Дозировки лекарственных препаратов', 3)
---
+++ sql/OKEISections.sql
@@ -0,0 +1,5 @@
+﻿INSERT [OKEISections] ([Id], [Name]) VALUES (1, N'Международные единицы измерения, включенные в ОКЕИ')
+INSERT [OKEISections] ([Id], [Name]) VALUES (2, N'Национальные единицы измерения, включенные в ОКЕИ')
+INSERT [OKEISections] ([Id], [Name]) VALUES (3, N'Четырехзначные национальные единицы измерения, включенные в ОКЕИ')
+
+
